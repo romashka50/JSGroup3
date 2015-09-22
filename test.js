6dd45b57d4c68c5b43b512e5dd2bf049de819eef
Roman <roma.buchuk@gmail.com>
2015-09-22 18:15:47 +0300

    added mongo and mongoose packages

--- test.js
+++ test.js
@@ -4,6 +4,29 @@ var app = express();
 
 var port = process.env.PORT || 3030;
 
+var mongoose = require('mongoose');
+
+var db1 = mongoose.createConnection('localhost', 'myTestDb', 27017, {server: {poolSize: 10}});
+var db2 = mongoose.createConnection('localhost', 'test', 27017, {server: {poolSize: 10}});
+
+
+
+db1.on('error', function(err){
+    console.error(err);
+});
+
+db1.once('open', function(){
+    console.log('connected');
+});
+
+db2.on('error', function(err){
+    console.error(err);
+});
+
+db2.once('open', function(){
+    console.log('connected');
+});
+
 //'============ Load Routes ==============';
 require('./routes')(app);
 //'============ Load Routes ==============';
