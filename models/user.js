23b0756d72aa8e736ec16a777f329baac768666c
darkvas <darkvas90@gmail.com>
2015-09-24 18:02:33 +0300

    added remove dependency posts for user

--- models/user.js
+++ models/user.js
@@ -12,7 +12,21 @@ module.exports = function (postGre) {
         },
 
         removeAllDependencies: function (model) {
-
+            postGre.Models.Post
+                .query(function (qb) {
+                    qb.where('user_id', '=', model.id);
+                })
+                .fetchAll()
+                .then(function (posts) {
+                    for (var i = posts.length - 1; i >= 0; i--) {
+                        posts.models[i]
+                            .destroy()
+                            .then()
+                            .otherwise(function (err) {
+                                console.log('err on delete user posts: ' + err);
+                            })
+                    }
+                });
         }
     });
 
